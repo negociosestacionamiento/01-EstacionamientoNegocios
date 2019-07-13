8265e9e60c83abb9304033ea06d634f8efc82b38
marioargueta150@gmail.com <marioargueta150@gmail.com>
2019-07-13 16:46:10 -0600

    Modificacion de la clase Vehiculo

--- Clases_Datos/Datos_Vehiculo.cs
+++ Clases_Datos/Datos_Vehiculo.cs
@@ -11,6 +11,39 @@ namespace Clases_Datos
 {
     class Datos_Vehiculo
     {
+        private int idVehiculo;
+        private string placa;
+        private int idTipoVehiculo;
 
+        SqlConnection Con = new SqlConnection(Conexion.Cn);
+
+        public Datos_Vehiculo()
+        {
+
+        }
+
+        public int IdVehiculo { get { return idVehiculo; } set { idVehiculo = value;} }
+        public string Placa { get { return placa; } set { placa = value; } }
+        public int IdTipoVehiculo { get { return idTipoVehiculo; } set { idTipoVehiculo = value; } }
+
+        public DataTable MostrarVehiculo()
+        {
+            DataTable TablaVehiculo = new DataTable();
+            try
+            {
+                SqlCommand Cmd = new SqlCommand("SP_DesplegarEstacionamientoVehiculo", Con);
+                Cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter sqlData = new SqlDataAdapter(Cmd);
+                using (sqlData)
+                {
+                    sqlData.Fill(TablaVehiculo);
+                }
+            }
+            catch (Exception)
+            {
+                TablaVehiculo = null;
+            }
+            return TablaVehiculo;
+        }
     }
 }
