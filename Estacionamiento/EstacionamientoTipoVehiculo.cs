b41cf436cdb8d74466972ddc2452efbd366555f7
Camila Sampson <camilagsampr@gmail.com>
2019-07-13 12:27:35 -0600

    Conexion, prueba tabla tipo vehiculo

---
+++ Estacionamiento/EstacionamientoTipoVehiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using Clases_Datos;
+
+namespace Estacionamiento
+{
+    public class EstacionamientoTipoVehiculo
+    {
+        public static DataTable MostrarTipoVehiculo()
+        {
+            return new Datos_TipoVehiculo().MostrarTipoVehiculo();
+        }
+    }
+}
---
+++ Pantallas/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Estacionamiento;
+using System.Data;
+
+namespace Pantallas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+            DataTable dt = EstacionamientoTipoVehiculo.MostrarTipoVehiculo();
+
+            IList<Tipos> V = new List<Tipos>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                V.Add(new Tipos
+                {
+                    nombre = dr[0].ToString(),
+
+                });
+            }
+            lbTipoVehiculo.ItemsSource = V;
+
+        }
+
+        public class Tipos
+        {
+            public string nombre { get; set; }
+        }
+    }
+
+}
